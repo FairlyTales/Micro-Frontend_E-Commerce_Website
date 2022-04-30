960fca7ec15b0c2f34c1cc1acc3b297fddbedbfc
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-30 22:18:41 +0300

    restored default header;

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -1,22 +1,14 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Footer from 'home/Footer';
+import Header from 'home/Header';
 import './index.scss';
 
-const Header = lazy(() => import('home/Header'));
-
 const App = () => {
-  const [showHeader, setShowHeader] = useState(false);
-
   return (
     <div className='text-3xl mx-auto max-w-6xl'>
-      {showHeader && (
-        <Suspense fallback={<div>Loading...</div>}>
-          <Header />
-        </Suspense>
-      )}
+      <Header />
       <div className='my-10'>PDP content</div>
-      <button onClick={() => setShowHeader(true)}>Show the header</button>
       <Footer />
     </div>
   );
