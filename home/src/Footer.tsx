60c763e9938ecd8fa1d6813e57cfb403d9c7d279
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-30 17:48:35 +0300

    added header and footer in home page project

---
+++ home/src/Footer.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const Footer: FC = () => {
+  return (
+    <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
+       Some footer info
+    </div>
+  );
+};
---
+++ home/src/Header.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+export const Header: FC = () => {
+  return (
+    <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
+      Just Another E-Commerce Store
+    </div>
+  );
+};
