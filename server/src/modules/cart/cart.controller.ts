7d1a1fa2f997fdc680575126ae6c77eb02bde3d7
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-05-02 22:02:17 +0300

    added POST method to cart controller;

--- server/src/modules/cart/cart.controller.ts
+++ server/src/modules/cart/cart.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import products, { Product } from '../../products';
 import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
@@ -34,5 +34,23 @@ export class CartController {
     return this.carts[req.user.userId] ?? { cartItems: [] };
   }
 
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  async create(@Request() req, @Body() { id }: { id: string }): Promise<Cart> {
+    const cart = this.carts[req.user.userId];
+    const cartItem = cart.cartItems.find(
+      cartItem => cartItem.id === parseInt(id),
+    );
 
+    if (cartItem) {
+      cartItem.quantity += 1;
+    } else {
+      cart.cartItems.push({
+        ...products.find(product => product.id === parseInt(id)),
+        quantity: 1,
+      });
+    }
+
+    return cart;
+  }
 }
