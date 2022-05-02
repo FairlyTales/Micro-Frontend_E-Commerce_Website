15fd4747d1d7b943aa8d043b777f6b846a8cd91d
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-05-02 20:37:46 +0300

    added product info page content and fetch logic;

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -3,19 +3,23 @@ import ReactDOM from 'react-dom';
 import Footer from 'home/Footer';
 import Header from 'home/Header';
 import { ProductInfo } from './ProductInfo';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import './index.scss';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <div className='text-3xl mx-auto max-w-6xl'>
-      <Header />
-      <div className='my-10'>
-        <ProductInfo />
+    <QueryClientProvider client={queryClient}>
+      <div className='text-3xl mx-auto max-w-6xl'>
+        <Header />
+        <div className='my-10'>
+          <ProductInfo />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </QueryClientProvider>
   );
 };
-
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- pdp/src/ProductInfo.tsx
+++ pdp/src/ProductInfo.tsx
@@ -1,24 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { getProductById, Product } from 'home/products';
+import React from 'react';
+import { currency, getProductById } from 'home/products';
+import { useQuery } from 'react-query';
 
 export const ProductInfo = () => {
-  const [product, setProduct] = useState<Product | null>(null);
   const id = 1;
 
-  useEffect(() => {
-    if (id) {
-      getProductById(id).then((product: Product) => setProduct(product));
-    } else {
-      setProduct(null);
-    }
-  }, [id]);
+  const { data: product, status } = useQuery('product', () => getProductById(id));
 
-  if (!product) return null;
+  if (status === 'loading') return <p>Loading Product...</p>;
+  if (status === 'error' || !product) return <p>Something went wrong. We are working on it!</p>;
 
   return (
-    <div className='grid grid-cols-2 gap-5'>
-      <div>
-        <img src={product.image} alt={product.name} />
+    <div className='my-10 px-10'>
+      <div className='grid grid-cols-2 gap-5'>
+        <div className='flex justify-center align-middle'>
+          <img src={product.image} alt={product.name} />
+        </div>
+        <div>
+          <div className='flex'>
+            <h1 className='font-bold text-3xl flex-grow'>{product.name}</h1>
+            <div className='font-bold text-3xl flex-end'>
+              {currency.format(product.price)}
+            </div>
+          </div>
+          <div className='mt-10'>{product.description}</div>
+          <div className='mt-10'>{product.longDescription}</div>
+        </div>
       </div>
     </div>
   );
