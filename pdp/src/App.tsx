7ca7738003cc5cff891f524f5c24f7b225a35068
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-05-02 20:48:19 +0300

    added routing for PDP;

--- pdp/src/App.tsx
+++ pdp/src/App.tsx
@@ -4,21 +4,27 @@ import Footer from 'home/Footer';
 import Header from 'home/Header';
 import { ProductInfo } from './ProductInfo';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import './index.scss';
 
 const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <div className='text-3xl mx-auto max-w-6xl'>
-        <Header />
-        <div className='my-10'>
-          <ProductInfo />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <div className='text-3xl mx-auto max-w-6xl'>
+          <Header />
+          <div className='my-10'>
+            <Routes>
+              <Route path='/product/:id' element={<ProductInfo />} />
+            </Routes>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
-    </QueryClientProvider>
+      </QueryClientProvider>
+    </Router>
   );
 };
 
--- pdp/src/ProductInfo.tsx
+++ pdp/src/ProductInfo.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { currency, getProductById } from 'home/products';
 import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 
 export const ProductInfo = () => {
-  const id = 1;
+  const { id } = useParams();
 
-  const { data: product, status } = useQuery('product', () => getProductById(id));
+  const { data: product, status } = useQuery('product', () => getProductById(Number(id)));
 
   if (status === 'loading') return <p>Loading Product...</p>;
   if (status === 'error' || !product) return <p>Something went wrong. We are working on it!</p>;
+
 
   return (
     <div className='my-10 px-10'>
