69237b097ea58d68496470898968e32d3911f9d1
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-05-03 17:46:38 +0300

    added get, add and clear  cart methods;

--- cart/src/cart.ts
+++ cart/src/cart.ts
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 const API = 'http://localhost:8080';
 
 export const jwt = new BehaviorSubject(null);
+export const cart = new BehaviorSubject(null);
 
 export const useLoggedIn = () => {
   const [loggedIn, setLoggedIn] = useState<boolean>(Boolean(jwt.value));
@@ -15,7 +16,47 @@ export const useLoggedIn = () => {
   return loggedIn;
 };
 
-export const login = (username: string, password: string) =>
+export const getCart = () =>
+  fetch(`${API}/cart`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${jwt.value}`,
+    },
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      cart.next(res);
+      return res;
+    });
+
+export const addToCart = (id: string) =>
+  fetch(`${API}/cart`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${jwt.value}`,
+    },
+    body: JSON.stringify({ id }),
+  })
+    .then((res) => res.json())
+    .then(() => {
+      getCart();
+    });
+
+export const clearCart = () =>
+  fetch(`${API}/cart`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${jwt.value}`,
+    },
+  })
+    .then((res) => res.json())
+    .then(() => {
+      getCart();
+    });
+
+export const login = (username: string, password: string): Promise<string> =>
   fetch(`${API}/auth/login`, {
     method: 'POST',
     headers: {
@@ -30,8 +71,6 @@ export const login = (username: string, password: string) =>
     .then((data) => {
       // updating jwt content with a received token
       jwt.next(data.access_token);
-      // getCart();
+      getCart();
       return data.access_token;
     });
-
-
